2d6723d781e25ea7e64c5271d82090b325413cf3
Sangeeth R <47264999+DRIRU@users.noreply.github.com>
2025-07-03 14:25:59 +0530

    Updated request.py

--- backend/app/db/models/request.py
+++ backend/app/db/models/request.py
@@ -3,8 +3,6 @@ from ..database import Base
 from sqlalchemy.orm import relationship
 from datetime import datetime
 
-Base = declarative_base()
-
 class RequestUrgencyEnum(Enum):
     low = "low"
     normal = "normal"
